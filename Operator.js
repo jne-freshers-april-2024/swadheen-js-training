e2124aadf602a08334ecabe4d915899c362dc96b
swadhe <swadheen.sethy@thinkitive.com>
2024-04-24 12:03:09 +0530

    second commit

---
+++ Operator.js
@@ -0,0 +1,18 @@
+let a=10;
+let b=20;
+console.log(a+b);
+
+//Subtraction
+let c=30;
+let d=40;
+console.log(c-d);
+
+//multiplication
+let a1=10;
+let a2=20;
+console.log(a1*a2);
+
+//Division
+let a3=30;
+let a4=15;
+console.log(a3/a4);
